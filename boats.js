2bd65317903f1b18d23e46e3fc65c8c1fede57a6
k0tox <kazua01fn@outlook.com>
2026-03-06 10:52:50 -0600

    Update boats.js

--- boats.js
+++ boats.js
@@ -1,1 +1,9 @@
+// boats.js
+export const boats = [
+    { id: 1, name: "Row Boat", speed: 2 },
+    { id: 2, name: "Motor Boat", speed: 5 },
+];
 
+export function getBoatById(id) {
+    return boats.find(b => b.id === id);
+}
