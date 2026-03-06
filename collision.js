87b6396a88a3e6af9a7ce7a4f30d72d9058f66b3
k0tox <kazua01fn@outlook.com>
2026-03-06 11:30:23 -0600

    Update collision.js

--- collision.js
+++ collision.js
@@ -1,1 +1,9 @@
-
+// collision.js
+export function checkCollision(a, b) {
+    return !(
+        a.x > b.x + b.width ||
+        a.x + a.width < b.x ||
+        a.y > b.y + b.height ||
+        a.y + a.height < b.y
+    );
+}
