e10a2ded1558e8ef521170b040249812e4f54d9a
k0tox <kazua01fn@outlook.com>
2026-03-06 11:33:34 -0600

    Update controls.js

--- controls.js
+++ controls.js
@@ -1,1 +1,9 @@
+// controls.js
+export const keysPressed = {};
 
+window.addEventListener("keydown", e => keysPressed[e.key.toLowerCase()] = true);
+window.addEventListener("keyup", e => keysPressed[e.key.toLowerCase()] = false);
+
+export function isKeyPressed(key) {
+    return !!keysPressed[key.toLowerCase()];
+}
