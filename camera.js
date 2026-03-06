ec1747d98fafbdffb3c5941f6f499970d4037601
k0tox <kazua01fn@outlook.com>
2026-03-06 10:53:02 -0600

    Update camera.js

--- camera.js
+++ camera.js
@@ -1,1 +1,15 @@
+// camera.js
+export const camera = {
+    x: 0,
+    y: 0,
+    zoom: 1
+};
 
+export function moveCamera(dx, dy) {
+    camera.x += dx;
+    camera.y += dy;
+}
+
+export function setZoom(z) {
+    camera.zoom = z;
+}
