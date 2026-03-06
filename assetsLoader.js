53a87b9d483d3e9942fc6de7563225c0cabdde5b
k0tox <kazua01fn@outlook.com>
2026-03-06 10:51:23 -0600

    Update assetsLoader.js

--- assetsLoader.js
+++ assetsLoader.js
@@ -1,1 +1,20 @@
+// assetsLoader.js
+export const assets = {};
 
+export async function loadAssets() {
+    // Example: load images, 3D models, textures
+    const images = [
+        { name: "fisch", src: "textures/fisch.png" },
+        { name: "food", src: "textures/food.png" },
+    ];
+
+    for (let img of images) {
+        const image = new Image();
+        image.src = img.src;
+        await new Promise(res => {
+            image.onload = res;
+        });
+        assets[img.name] = image;
+    }
+    console.log("Assets loaded", assets);
+}
