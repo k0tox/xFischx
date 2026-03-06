2038bf427140a7d0a033c2728639d0689585f37d
k0tox <kazua01fn@outlook.com>
2026-03-06 11:10:23 -0600

    Update boats.js

--- boats.js
+++ boats.js
@@ -1,7 +1,7 @@
 // boats.js
 export const boats = [
-    { id: 1, name: "Row Boat", speed: 2 },
-    { id: 2, name: "Motor Boat", speed: 5 },
+    { id: 1, name: "Row Boat", speed: 2, capacity: 2, texture: "boat_row" },
+    { id: 2, name: "Motor Boat", speed: 5, capacity: 4, texture: "boat_motor" },
 ];
 
 export function getBoatById(id) {
