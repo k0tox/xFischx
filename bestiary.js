20b53ad0ef16cd464e01fd1607cbdb201ab86a82
k0tox <kazua01fn@outlook.com>
2026-03-06 10:52:41 -0600

    Update bestiary.js

--- bestiary.js
+++ bestiary.js
@@ -1,1 +1,13 @@
+// bestiary.js
+export const fishList = [
+    { id: 1, name: "Common Fisch", rarity: "common", size: "small" },
+    { id: 2, name: "Golden Fisch", rarity: "rare", size: "medium" },
+];
 
+export function getFishById(id) {
+    return fishList.find(f => f.id === id);
+}
+
+export function logAllFish() {
+    console.table(fishList);
+}
