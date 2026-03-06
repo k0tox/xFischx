a957bbfd4e36148af7b72c1add9dd95013307b79
k0tox <kazua01fn@outlook.com>
2026-03-06 11:33:57 -0600

    Update dataLoader.js

--- dataLoader.js
+++ dataLoader.js
@@ -1,1 +1,6 @@
-
+// dataLoader.js
+export async function loadJSON(path) {
+    const res = await fetch(path);
+    if (!res.ok) throw new Error(`Failed to load ${path}`);
+    return await res.json();
+}
