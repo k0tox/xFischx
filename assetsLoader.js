5dd2b3e7dd0c161fc96c85c1a31ae3362dcd3424
k0tox <kazua01fn@outlook.com>
2026-03-06 11:08:48 -0600

    Update assetsLoader.js

--- assetsLoader.js
+++ assetsLoader.js
@@ -2,19 +2,22 @@
 export const assets = {};
 
 export async function loadAssets() {
-    // Example: load images, 3D models, textures
     const images = [
         { name: "fisch", src: "textures/fisch.png" },
-        { name: "food", src: "textures/food.png" },
+        { name: "rod_basic", src: "textures/rod_basic.png" },
+        { name: "rod_advanced", src: "textures/rod_advanced.png" },
+        { name: "bait_worm", src: "textures/bait_worm.png" },
+        { name: "bait_shrimp", src: "textures/bait_shrimp.png" },
+        { name: "grass", src: "textures/grass.png" },
+        { name: "water", src: "textures/water.png" },
+        { name: "boat_row", src: "textures/boat_row.png" }
     ];
 
-    for (let img of images) {
+    for (const img of images) {
         const image = new Image();
         image.src = img.src;
-        await new Promise(res => {
-            image.onload = res;
-        });
+        await new Promise(resolve => image.onload = resolve);
         assets[img.name] = image;
     }
-    console.log("Assets loaded", assets);
+    console.log("All assets loaded:", Object.keys(assets));
 }
