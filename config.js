a52a8f5ff20d920cde5fa0af5d9f9dac825422a8
k0tox <kazua01fn@outlook.com>
2026-03-06 11:32:23 -0600

    Update config.js

--- config.js
+++ config.js
@@ -1,1 +1,11 @@
-
+// config.js
+export const CONFIG = {
+    WIDTH: window.innerWidth,
+    HEIGHT: window.innerHeight,
+    FPS: 60,
+    PLAYER_SPEED: 5,
+    PLAYER_SIZE: 50,
+    FISH_SPAWN_RATE: 5,
+    WEATHER_INTERVAL: 60,
+    DAY_LENGTH: 300
+};
